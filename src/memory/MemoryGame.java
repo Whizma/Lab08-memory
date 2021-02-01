20825b1a19e83d234340a43daffbb1ad5992426e
Isak <isak@maatta.se>
2021-02-01 23:00:39 +0100

    vigge

--- src/memory/MemoryGame.java
+++ src/memory/MemoryGame.java
@@ -1,16 +1,3 @@
-<<<<<<< HEAD
-package memory;
-
-public class MemoryGame {
-	public static void main(String[] args) {
-		String[] frontFileNames = {"can.jpg", "flopsy_mopsy_cottontail.jpg",
-				"friends.jpg", "mother_ladybird.jpg", "mr_mcgregor.jpg",
-				"mrs_rabbit.jpg", "mrs_tittlemouse.jpg", "radishes.jpg" };
-		
-		// Fyll i egen kod här
-	}
-}
-=======
 package memory;
 
 public class MemoryGame {
@@ -35,4 +22,3 @@ public class MemoryGame {
 		// Fyll i egen kod här
 	}
 }
->>>>>>> c286feb629bca7f2fa7e95954caa43a9be84decf
